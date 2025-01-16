d1b8ac0033348d9e33b5d4bf98557f04b0eba2c4
Jie Xu <jie.xu@image-metrics.com>
2025-01-16 09:44:01 -0800

    update

--- export_to_obsidian.py
+++ export_to_obsidian.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
         paper_string += f"### Date Added: {data_str}\n\n"
         paper_string += f"### Paper link \n[{file_name}](https://arxiv.org/abs/{paper['arxiv_id']})\n\n"        
         if len(urls) > 0 and 'github' in urls[0]:
-          paper_string += f"### Github repo \n[{file_name}]({urls[0].rstrip('.,')})\n\n"        
+          paper_string += f"### Github repo \n[{file_name}]({urls[0].rstrip('.,}')})\n\n"        
 
         paper_string += '### Abstract\n'
         paper_string += f"{abstract_str}"
